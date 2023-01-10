69b1ffb496236895871170cba23e15aafeb94a84
bmhungqb <21522110@gm.uit.edu.vn>
2023-01-10 16:12:38 +0700

    #25

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,5 @@
-
 import db from "../models/index";
+import CRUDService from "../services/CRUDService";
 let getHomePage = async (req, res) => {
     try {
         let data = await db.User.findAll();
@@ -10,6 +10,23 @@ let getHomePage = async (req, res) => {
         console.log(e)
     }
 }
+
+let getAboutPage = async (req, res) => {
+    return res.render("tes");
+}
+
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs');
+}
+
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    return res.send('hihi post CRUD');
+}
+
 module.exports = {
     getHomePage: getHomePage,
+    getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD
 }
