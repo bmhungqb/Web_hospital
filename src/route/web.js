bcb55db9bfcf54b7ad4b025b55bb9cd471ea4190
bmhungqb <21522110@gm.uit.edu.vn>
2023-01-10 18:02:28 +0700

    #27

--- src/route/web.js
+++ src/route/web.js
@@ -10,7 +10,9 @@ let initWebRoutes = (app) => {
     router.get("/crud", homeController.getCRUD);
     router.post("/post-crud", homeController.postCRUD);
     router.get("/get-crud", homeController.displayGetCRUD);
-    return app.use("/", router)
+    router.get("/edit-crud", homeController.getEditCRUD);
+    router.post("/put-crud", homeController.putCRUD);
+    return app.use("/", router);
 }
 
 module.exports = initWebRoutes;
