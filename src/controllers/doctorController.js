c293b0b19dec54c0be46bff6d6f08bfcc954e1df
bmhungqb <21522110@gm.uit.edu.vn>
2023-02-24 17:26:48 +0700

    #83

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -87,6 +87,18 @@ let getExtraInforDoctorById = async (req, res) => {
         })
     }
 }
+
+let getProfileDoctorById = async (req, res) => {
+    try {
+        let infor = await doctorService.getProfileDoctorById(req.query.doctorId);
+        return res.status(200).json(infor)
+    } catch (e) {
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+}
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
@@ -95,4 +107,5 @@ module.exports = {
     bulkCreateSchedule: bulkCreateSchedule,
     getScheduleByDate: getScheduleByDate,
     getExtraInforDoctorById: getExtraInforDoctorById,
+    getProfileDoctorById: getProfileDoctorById,
 }
